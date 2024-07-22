27708e1ab2d57dcf74d33c37ca8d1394abf70b22
ToanG1 <Toandinh01675@gmail.com>
2024-07-22 23:32:20 +0700

    radial

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="bg-dark-purple">
+      <div className="bg-gradient-color bg-gradient-1 h-60 mb-20">box 1</div>
+      <div className="bg-gradient-color bg-gradient-2 h-60 mb-20">box 2</div>
+      <div className="bg-gradient-color bg-gradient-3 h-60 mb-20">box 3</div>
+      <div className="bg-gradient-color bg-gradient-4 h-60 mb-20">box 4</div>
+      <div className="bg-radial-1 h-60 mb-20">radial</div>
+    </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+          'gradient-1': 'linear-gradient(135deg, #200C3B 50%, #4A2F6B 85%, #674687 100%)',
+          'gradient-2': 'linear-gradient(105deg, #200C3B 20%, #311C52 25%, #392061 30%, #311C52 38%, #1E0C37 45%)',
+          'gradient-3': 'linear-gradient(110deg, #200C3B 10%, #311C52 15%, #392061 23%, #311C52 32%, #1E0C37 45%)',
+          'gradient-4': 'linear-gradient(170deg, #200C3B 54%, #3A215F 72%, #6230B1 100%)',
+          'radial-1': 'radial-gradient(circle, rgba(118, 60, 172, 1) 0%, rgba(50, 15, 133, 0) 60%)'
+      },
+      colors: {
+        'dark-purple': '#1E0C37',
+        'purple-1': '#7127BA',
+        'purple-2': '#693B93',
+        'purple-3': '#A362FF',
+        'purple-4': '#9857D3',
+        'purple-5': '#763CAC',
+        'txt-primary': '#fff',
+        'txt-secondary': '#CCD6F6',
+        'company-bg': '#C71515',
+        'gradient-color': '#1E0C37',
+        'radial-color': 'rgba(118, 60, 172, 0.2)'
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
