0f3657dc185d861672923575ea112e599e3835e4
ToanG1 <Toandinh01675@gmail.com>
2024-07-28 16:45:16 +0700

    about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,45 @@
+import Box1 from "@/components/main/workingExperiences/box1";
+import Box2 from "@/components/main/workingExperiences/box1";
+import Box3 from "@/components/main/workingExperiences/box1";
+import Box4 from "@/components/main/workingExperiences/box1";
+
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="bg-dark-purple">
-      <div className="bg-gradient-color bg-gradient-1 h-60 mb-20">box 1</div>
-      <div className="bg-gradient-color bg-gradient-2 h-60 mb-20">box 2</div>
-      <div className="bg-gradient-color bg-gradient-3 h-60 mb-20">box 3</div>
-      <div className="bg-gradient-color bg-gradient-4 h-60 mb-20">box 4</div>
-      <div className="bg-radial-1 h-60 mb-20">radial</div>
+    <main className="bg-dark-purple font-preahvihear">
+      <div id="about-me">
+        <div id="profile" className="flex">
+          <div className="bg-radial-1 h-96 w-[21.5rem] relative">
+            <Image
+              src="/images/portfolio/avatar.png"
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+              alt="avatar"
+              width={200}
+              height={200}
+            />
+          </div>
+          <div id="slogan" className="text-txt-primary justify-center">
+            <h3 className="text-2xl">A Developer who</h3>
+            <h1 className="w-[30rem] leading-[5rem] text-6xl">
+              Judge a book by its
+              <strong className="text-purple-1"> cover </strong>...
+            </h1>
+            <h4>Because if the cover does not impress you what else can?</h4>
+          </div>
+        </div>
+        <div id="about"></div>
+      </div>
+      <div id="work-experiences">
+        <Box1 />
+        <Box2 />
+        <Box3 />
+        <Box4 />
+      </div>
+      <div id="technologies"></div>
+      <div id="projects"></div>
+      <div id="contacts"></div>
+      <div className="bg-radial-1 h-96 w-60 "></div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,15 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        preahvihear: ['Preahvihear', 'sans-serif'],
+      },
       backgroundImage: {
           'gradient-1': 'linear-gradient(135deg, #200C3B 50%, #4A2F6B 85%, #674687 100%)',
           'gradient-2': 'linear-gradient(105deg, #200C3B 20%, #311C52 25%, #392061 30%, #311C52 38%, #1E0C37 45%)',
           'gradient-3': 'linear-gradient(110deg, #200C3B 10%, #311C52 15%, #392061 23%, #311C52 32%, #1E0C37 45%)',
           'gradient-4': 'linear-gradient(170deg, #200C3B 54%, #3A215F 72%, #6230B1 100%)',
-          'radial-1': 'radial-gradient(circle, rgba(118, 60, 172, 1) 0%, rgba(50, 15, 133, 0) 60%)'
+          'radial-1': 'radial-gradient(ellipse, rgba(118, 60, 172, 1) 0%, rgba(50, 15, 133, 0) 60%)'
       },
       colors: {
-        'dark-purple': '#1E0C37',
+        'dark-purple': '#11071F',
         'purple-1': '#7127BA',
         'purple-2': '#693B93',
         'purple-3': '#A362FF',
