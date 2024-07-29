fbf521e72eeb7f72d649d0f2f4a855a91876d30b
ToanG1 <Toandinh01675@gmail.com>
2024-07-29 23:29:49 +0700

    responsive about

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Dinh Van Toan Portfolio",
   description: "Developer portfolio of Dinh Van Toan",
+  viewport: "width=device-width, initial-scale=1.0",
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,25 +6,45 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-dark-purple font-preahvihear">
+    <main className="bg-dark-purple font-preahvihear px-2 sm:px-[10%] md:py-[5%]">
       <div id="about-me">
-        <div id="profile" className="flex">
-          <div className="bg-radial-1 h-96 w-[21.5rem] relative">
+        <div
+          id="profile"
+          className="flex items-center w-full h-96 text-txt-primary"
+        >
+          <div className="bg-radial-1 h-[70%] md:h-full w-[40%] md:w-2/3 lg:w-1/2 xl:w-1/4 relative">
+            <div className="flex absolute -right-44 md:-right-32 xl:-right-44 top-10 md:top-2 xl:top-10 2xl:top-4">
+              <Image
+                src="/images/portfolio/arrow.png"
+                className="w-1/4 md:w-1/3 mt-2 md:mt-3 mr-1 md:mr-2"
+                alt="Who am I ?"
+                width={110}
+                height={110}
+              />
+              <span className="text-xs xl:text-xl md:text-base">
+                Hello! I am <strong className="text-purple-1">Van Toan</strong>
+              </span>
+            </div>
+
             <Image
               src="/images/portfolio/avatar.png"
-              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+              className="w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
               alt="avatar"
               width={200}
               height={200}
             />
           </div>
-          <div id="slogan" className="text-txt-primary justify-center">
-            <h3 className="text-2xl">A Developer who</h3>
-            <h1 className="w-[30rem] leading-[5rem] text-6xl">
+          <div id="slogan" className="w-1/3 md:w-1/2 xl:w-2/3 mt-2">
+            <h3 className="text-[10p] md:xll-base xl:text-2xl">
+              A Developer who
+            </h3>
+            <h1 className="xl:leading-[4rem] text-base md:text-4xl xl:text-6xl mb-3 md:pb-0 xl:w-2/3">
               Judge a book by its
               <strong className="text-purple-1"> cover </strong>...
             </h1>
-            <h4>Because if the cover does not impress you what else can?</h4>
+            <h4 className="text-[8px] md:text-xs">
+              Because if the cover does not impress you what else can?
+            </h4>
           </div>
         </div>
         <div id="about"></div>
